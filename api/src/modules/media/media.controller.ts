0497861d599a42bee197b522e88c4121700c06c5
FvCastr0 <fvdc2008@gmail.com>
2026-01-23 10:06:02 -0300

    update media controller

--- api/src/modules/media/media.controller.ts
+++ api/src/modules/media/media.controller.ts
@@ -1,32 +1,18 @@
-import { Controller, Get, Param, Res } from "@nestjs/common";
+import { Controller, Get, NotFoundException, Param, Res } from "@nestjs/common";
 import type { Response } from "express";
 import { existsSync } from "fs";
-import { extname, join } from "path";
+import { join } from "path";
 
 @Controller("media")
 export class MediaController {
-  @Get("media/:file")
-  getMedia(@Param("file") file: string, @Res() res: Response) {
-    const filePath = join(process.cwd(), "uploads", file);
+  @Get("/:filename")
+  getMedia(@Param("filename") filename: string, @Res() res: Response) {
+    const basePath = join(process.cwd(), "uploads");
+    const filePath = join(basePath, filename);
 
     if (!existsSync(filePath)) {
-      return res.sendStatus(404);
+      throw new NotFoundException("Arquivo não encontrado");
     }
-
-    const ext = extname(filePath);
-
-    const mimeMap: Record<string, string> = {
-      ".jpg": "image/jpeg",
-      ".jpeg": "image/jpeg",
-      ".png": "image/png",
-      ".webp": "image/webp",
-      ".mp3": "audio/mpeg",
-      ".ogg": "audio/ogg",
-      ".wav": "audio/wav",
-      ".mp4": "video/mp4"
-    };
-
-    res.setHeader("Content-Type", mimeMap[ext] ?? "application/octet-stream");
 
     return res.sendFile(filePath);
   }
