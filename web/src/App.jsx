066e2c7e302dabeedc3f08e665ae87b6a2e5aa5e
FvCastr0 <fvdc2008@gmail.com>
2026-01-28 11:35:05 -0300

    lading page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import fihass from "./assets/fihass.png";
+import match from "./assets/match.png";
+import smatch from "./assets/smatch.png";
 
 function App() {
   return (
-    <main>
-      <div>
+    <main className="container">
+      <div className="texts">
         <h1>O match perfeito entre fome e sabor começa agora</h1>
         <h2>
           <strong>Um clique</strong> te separa da melhor rede delivery de Juiz
@@ -12,8 +15,13 @@ function App() {
         <h3>Escolha sua marca e peça sem medo 🚀</h3>
       </div>
 
-      <div>
+      <div className="brands">
         <h1>Nossas marcas</h1>
+        <div className="images">
+          <img src={smatch} alt="Smatch Burger Logo" />
+          <img src={match} alt="Smatch Burger Logo" />
+          <img src={fihass} alt="Smatch Burger Logo" />
+        </div>
       </div>
     </main>
   );
